Fixed data leakage and bugs

Multilevel.py
@@ -6,11 +6,14 @@ from sklearn.model_selection import train_test_split
 #import neuralNetwork
 import time
 import numpy as np
-
+from neuralNetwork import neuralNetwork
+from Evaluate import Evaluate
+from testNeuralNetwork import testNetwork
 
 def Multilevel(data, dataName, max_ite=1, prop=0.8, multilevel=1, n_neighbors=10,
-    Upperlim=500, Imb_size=300, Model_Selec=1, numBorderPoints=10,  loss="cross", Level_size=1, refineMethod="border",
-    epochs=100, patienceLevel=2, weights=False, label=""):
+               Upperlim=500, Imb_size=300, Model_Selec=1, numBorderPoints=10,  loss="cross", alpha=0.5, gamma=4,
+               Level_size=1, refineMethod="border", epochs=100, batch_size=32, Dropout=0.3, batchnorm=0.2, factor=3, patienceLevel=2, weights=False, label=""):
+
     """
     The main function. Takes data and trains either a multilevel or traditional neural network
 
@@ -35,55 +38,79 @@ def Multilevel(data, dataName, max_ite=1, prop=0.8, multilevel=1, n_neighbors=10
     """
 
     data = pd.read_csv(data, index_col=False)
-    Pdata = data[data["Label"] == 1]
-    Ndata = data[data["Label"] == 2]
+    data["Label"] = np.where(data["Label"] == 2, 0, 1)
+
     Results = []
     totalTime = ()
 
     # Model Training Options
-    options = {"n_neighbors": n_neighbors, "Upperlim": Upperlim, "Model_Selec": Model_Selec, "Imb_size": Imb_size, "loss": loss,
-               "numBorderPoints": numBorderPoints, "refineMethod": refineMethod, "epochs": epochs,
-               "patienceLevel": patienceLevel, "weights": weights}
+    options = {"n_neighbors": n_neighbors, "Upperlim": Upperlim, "Model_Selec": Model_Selec, "Imb_size": Imb_size,
+               "loss": loss, "alpha": alpha, "gamma": gamma,"numBorderPoints": numBorderPoints,
+               "refineMethod": refineMethod, "epochs": epochs, "batch_size": batch_size, "Dropout": Dropout,
+               "BatchNorm": batchnorm,  "factor": factor, "patienceLevel": patienceLevel, "weights": weights}
 
     for ite in range(1, max_ite+1): 
         start = time.time()
 
-        Ntrainlbl, Ntraindata, Ntestlbl, Ntestdata = Split(Ndata, prop)
-        Ptrainlbl, Ptraindata, Ptestlbl, Ptestdata = Split(Pdata, prop)
-        
-        #Pweight = 1/len(Ptraindata)                      
-        #Nweight = 1/len(Ntraindata) 
+        # Create train/test data
+        traindata, testdata = train_test_split(data, train_size=0.7)
+        # Create train/validation data using the train dataset
+        traindata, valdata = train_test_split(traindata, train_size=0.7)
 
-        traindata = pd.concat([Ntraindata, Ptraindata])
-        train_l = pd.concat([Ntrainlbl, Ptrainlbl])
 
-        testdata = pd.concat([Ntestdata, Ptestdata])
-        test_l = pd.concat([Ntestlbl, Ptestlbl])
-
-        Best = {} # Will contain best results found. 
         if multilevel == 1:
+
+            Best = {}  # Will contain best results found.
+
+            Ptraindata = traindata[traindata["Label"] == 1]
+            Ntraindata = traindata[traindata["Label"] == 0]
+
+            Ptrainlbl = Ptraindata.drop(["Label"], axis=1)
+            Ntrainlbl = Ntraindata.drop(["Label"], axis=1)
+
+            traindata = pd.concat([Ntraindata, Ptraindata])
+            train_lbl = pd.concat([Ntrainlbl, Ptrainlbl])
+
+            # Validation is kept separate for determining when to stop refinement
+            val_lbl = valdata["Label"]
+            # Testing data is only used at the end of the algorithm
+            test_lbl = testdata["Label"]
 
             # Create the KNN graph that will be used in the finest layer of multilevel learning
             nNeighbors, nAdjMatrix = NearestNeighborSearch(Ntraindata, n_neighbors)
             pNeighbors, pAdjMatrix = NearestNeighborSearch(Ptraindata, n_neighbors)
 
-            # Put everything into a dictionary to better keep track of it all
+            # Put everything into a dictionary to keep all relevant info together
             negativeData = {"Data": Ntraindata, "Labels": Ntrainlbl, "KNeighbors": nNeighbors, "AdjMatrix": nAdjMatrix}
             positiveData = {"Data": Ptraindata, "Labels": Ptrainlbl, "KNeighbors": pNeighbors, "AdjMatrix": pAdjMatrix}
 
             level = 0
 
-            Results[ite],posBorderData, negBorderData, Level_size, trainedNetwork, options, Best, flag, Level_results =\
-                MLD(traindata, train_l, testdata, test_l,level, negativeData, positiveData, options)
+            model, posBorderData, negBorderData, Depth, options, Best, flag, Level_results =\
+                MLD(traindata, train_lbl, valdata, val_lbl,level, negativeData, positiveData, options)
 
-            """
-            depth = np.mean(Level_size)
-            
+            res = Evaluate(model, testdata, test_lbl)
+
+            aveCoarsenDepth = np.mean(Level_size)
 
         else:
-            Results[ite], trainedNetwork[ite], options[ite] = neuralNetwork(Ntestdata, Ptestdata)
-            depth = 0
 
+            train_lbl = traindata["Label"]
+            val_lbl = valdata["Label"]
+            test_lbl = testdata["Label"]
+
+            traindata = traindata.drop(["Label"], axis=1)
+            valdata = valdata.drop(["Label"], axis=1)
+            testdata = testdata.drop(["Label"], axis=1)
+
+            model = neuralNetwork(traindata, train_lbl, valdata, val_lbl, Model_Selec, options)
+            #model = testNetwork(traindata, train_lbl, valdata, val_lbl)
+            res = Evaluate(model, testdata, test_lbl)
+            Results.append(res)
+
+            aveCoarsenDepth = 0
+
+        """
         end = time.time()
         totalTime.append(start-end) 
 
@@ -102,7 +129,7 @@ def Multilevel(data, dataName, max_ite=1, prop=0.8, multilevel=1, n_neighbors=10
         'stdSen': Results.stdSen,  
         'stdSpec': Results.stdSpec, 
         'Time (sec)': averageTime, 
-        'Depth': depth, 
+        'Average Coarsening Depth': aveCoarsenDepth, 
         'Refined': Results.refined
     }, columns=['GMean','Acc', 'Sen', 'Spec','stdGMean','stdAcc','stdSen', 'stdSpec', 'Time (sec)', 'Depth', 'Refined'])
 
@@ -112,4 +139,4 @@ def Multilevel(data, dataName, max_ite=1, prop=0.8, multilevel=1, n_neighbors=10
 
 
 if __name__ == "__main__":
-    Multilevel(data="../Hypothyroid.csv", dataName="Hypothyroid")
+    Multilevel(data="../Hypothyroid.csv", dataName="Hypothyroid", multilevel=0)

neuralNetwork.py
@@ -6,6 +6,7 @@ from keras import backend as K
 import tensorflow as tf
 import tensorflow_addons as tfa
 from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
 import numpy as np
 import os
 
@@ -27,10 +28,11 @@ def weighted_binary_cross_entropy(weights: dict, from_logits: bool = False):
 
     return weighted_cross_entropy_fn
 
-def neuralNetwork(traindata,train_l,valdata,val_l, Model_Selec, 
-loss,epochs,weights, options, model=None):
+def neuralNetwork(traindata,train_l,valdata,val_l, Model_Selec, options, model=None):
+
     ''' Function for creating/training the neural network. If not initialized,
-    hyperparameter tuning is used to create the architecture. 
+    hyperparameter tuning is used to create the architecture. Otherwise,
+    The model passed to it is trained further on the new data.
     Inputs:
         <traindata>: The train data
         <train_l>: Training labels
@@ -45,13 +47,14 @@ loss,epochs,weights, options, model=None):
     Outputs:
         <model>: A trained model
     '''
-    # Tune architecture of model. 
+
+    # Tune architecture of model.
     if Model_Selec == 1:
-        
+
         inputSize = traindata.shape[1]
 
         # Class weights for balancing. 
-        class_weights = class_weight.compute_class_weight('balanced', np.unique(train_l), train_l)
+        class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(train_l), y=train_l)
         class_weight_dict = dict(enumerate(class_weights))
         pos = class_weight_dict[1]
         neg = class_weight_dict[0]
@@ -68,41 +71,27 @@ loss,epochs,weights, options, model=None):
             model.add(tf.keras.Input(shape=(inputSize,))) 
 
             # Define number of hidden layers/dropout/batchnumber and associated number of nodes per layer
-            for i in range(hp.Int('num_layers', 2, 8)):
+            for i in range(hp.Int('num_layers', 2, 6)):
                 units = hp.Choice('units_' + str(i), values=[30, 36, 30, 41, 45, 60])
-                deep_activation = hp.Choice('dense_activation_' + str(i), values=['relu', 'tanh'])
-                model.add(tf.keras.layers.Dense(units=units, activation=deep_activation))
-
-                if options['Dropout']:
-                    model.add(tf.keras.layers.Dropout(options['Dropout_Rate']))
-
-                if options['BatchNorm']:
-                    model.add(tf.keras.layers.BatchNormalization(momentum=options['Momentum']))
+                model.add(tf.keras.layers.Dense(units=units, activation='relu'))
+                model.add(tf.keras.layers.Dropout(options['Dropout']))
+                model.add(tf.keras.layers.BatchNormalization(momentum=options['BatchNorm']))
 
             # Add final layer
-            final_activation = 'softmax'
-            if options['bias_init']:
-                model.add(
-                    tf.keras.layers.Dense(2, activation=final_activation, bias_initializer=tf.keras.initializers.Constant(value=bias)))
-            else:
-                model.add(tf.keras.layers.Dense(2, activation=final_activation))
+            model.add(tf.keras.layers.Dense(1, activation='softmax'))
 
-                # Select optimizer
-                optimizer = hp.Choice('optimizer', values=[tf.keras.optimizers.Adam(lr, clipnorm=0.0001),
-                                                           tf.keras.optimizers.Nadam(lr, clipnorm=0.0001), 
-                                                           tf.keras.optimizers.RMSprop(lr, clipnorm=0.0001), 
-                                                           tf.keras.optimizers.SGD(lr, clipnorm=0.0001)])
-
+            # Select optimizer and learning rate.
             lr = hp.Choice('learning_rate', [1e-3, 1e-4, 1e-5])
+            optimizer = tf.keras.optimizers.Adam(lr, clipnorm=0.0001)
 
             # Loss function
             # TODO: Double check that this is the correct focal loss.
-            if options['focal']:
-                loss = tfa.losses.SigmoidFocalCrossEntropy(alpha=options['alpha'], gamma=options['gamma'])
-            elif options['class_weights']:
+            if options['loss'] == "focal":
+                loss = tfa.losses.SigmoidFocalCrossEntropy(alpha=options["alpha"], gamma=options["gamma"])
+            elif options['loss'] == "weighted":
                 loss = weighted_binary_cross_entropy(class_weight_dict)
             else:
-                loss = 'binary_crossentropy'
+                loss = 'categorical_crossentropy'
 
             # Compilation
             model.compile(optimizer=optimizer,
@@ -116,20 +105,24 @@ loss,epochs,weights, options, model=None):
 
         tuner = keras_tuner.Hyperband(build_model,
                                 objective=keras_tuner.Objective('val_auc', direction="max"),
-                                max_epochs=epochs,
+                                max_epochs=options["epochs"],
                                 seed=1234,
                                 factor=options["factor"],
                                 overwrite=True,
                                 directory=os.path.normpath('C:/'))
 
-        tuner.search(traindata, train_l, epochs=epochs, validation_data=(valdata, val_l))
+
+        tuner.search(traindata, train_l, epochs=options["epochs"], validation_data=(valdata, val_l))
         best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
         model = tuner.hypermodel.build(best_hps)
         
     else:
+
         # Continue Training with new data
-        model.fit(traindata, train_l, batch_size=options['batch_size'],
-                                      epochs=options['epochs'],
-                                      verbose=2)
+
+        callback = tf.keras.callbacks.EarlyStopping(monitor='val_auc', patience=5)
+
+        model.fit(traindata, train_l, validation_data=(valdata, val_l), batch_size=options["batch_size"],
+                  epochs=options["epochs"], callbacks=callback, verbose=2)
 
     return model