81cafe0053c66ede892d29384fd801cacde8eca4
rachellb <rachel.l.bennett-1@ou.edu>
2023-02-16 10:59:27 -0600

    Created separate function to evaluate model

---
+++ Evaluate.py
@@ -0,0 +1,26 @@
+
+
+from imblearn.metrics import geometric_mean_score, specificity_score
+from sklearn.metrics import confusion_matrix
+
+def Evaluate(model, data, labels):
+
+    prediction = (model.predict(data) > 0.5).astype("int32")
+    specificity = specificity_score(labels, prediction)
+    gmean = geometric_mean_score(labels, prediction)
+    score = model.evaluate(data, labels, verbose=0)
+    tn, fp, fn, tp = confusion_matrix(labels, prediction).ravel()
+
+    results = {"Loss": score[0],
+               "Accuracy": score[1],
+               "AUC": score[4],
+               "Gmean": gmean,
+               "Recall": score[3],
+               "Precision": score[2],
+               "Specificity": specificity,
+               "True Positives": tp,
+               "True Negatives": tn,
+               "False Positives": fp,
+               "False Negatives": fn}
+
+    return results
